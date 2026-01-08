736b1ce075cdd8edbb6d2f2ba69e32e042d2bb37
jjmartinmelero <juanjesusmelero@hotmail.com>
2026-01-08 10:31:54 +0100

    added several properties in TresMesh

--- lessons/starter/src/App.vue
+++ lessons/starter/src/App.vue
@@ -7,11 +7,23 @@ import TheExperience from './components/TheExperience.vue'
 <template>
   <main class="relative mx-auto h-screen flex items-center">
     <div class="w-full h-screen flex">
-      <section class="min-h-400px w-2/3">
-        <TresCanvas alpha>
+      <!-- <section class="min-h-400px w-2/3"> -->
+        <!-- <TresCanvas alpha>
           <TheExperience />
+        </TresCanvas> -->
+        <TresCanvas clear-color="#82dbc8">
+          <TresPerspectiveCamera :position="[0, 0, 5]" />
+          <TresMesh 
+            :position="[0.5,1,-1]" 
+            :rotation="[-Math.PI / 4,0,0]"
+            :scale="[0.5,0.5,0.5]">
+            <!-- <TresBoxGeometry :args="[1,2,3]" /> -->
+            <TresBoxGeometry/>
+            <TresMeshNormalMaterial />
+          </TresMesh>
+          <TresAxesHelper />
         </TresCanvas>
-      </section>
+      <!-- </section> -->
       <aside class="bg-dark text-gray shadow-md w-1/3">
         <header class="p-8 bg-dark-700">
           <h1>
